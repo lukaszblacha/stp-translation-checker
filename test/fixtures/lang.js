08af7f6823ecd742d79785d48aad9f28d862c92e
Lukasz Blacha <lukasz.blacha@schibsted.pl>
2015-01-09 09:17:11 +0100

    getting things to work

---
+++ test/fixtures/lang.js
@@ -0,0 +1,11 @@
+define("data/lang", [], function() {
+    var _dict = {
+        test : 'with space before colon',
+        'singlequote': 'some dummy translation',
+        'singlequote with whitespace': 'some dummy translation',
+        "doublequote": 'some dummy translation',
+        "doublequote with whitespace": 'some dummy translation',
+        "another unused key": 'dummy text'
+    };
+    return _dict;
+});
